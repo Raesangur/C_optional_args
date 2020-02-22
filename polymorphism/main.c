46dfdee787d3ca6ed7e3cd4653c27235b43b0d5a
Pascal-Emmanuel Lachance <pascalemmanuel.lachance@gmail.com>
2020-02-22 01:13:52 -0500

    Update main.c

--- polymorphism/main.c
+++ polymorphism/main.c
@@ -2,7 +2,8 @@
 
 void main(void)
 {
-    Foo("Hello World!", 10);
+    Foo("Hello World!", 10);    // Prints "Hello World" 10 times
+    Foo("Hello World!");        // Prints "Hello World" 5 times (parameterized default in foo.h
 
     while (1);
 }
