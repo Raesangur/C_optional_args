e19ba39ccac1f51388b793953bfab1110a47e5bf
Pascal-Emmanuel Lachance <pascalemmanuel.lachance@gmail.com>
2020-02-22 01:34:40 -0500

    Comments to the macro'd inline assembly
    To explain stack injection and post-checking

--- polymorphism/foo.h
+++ polymorphism/foo.h
@@ -1,18 +1,35 @@
 #pragma once
 #include <stdint.h>
 
-#define FOO_DEFAULT_SIZE        5
-
-#define Foo(...)                {\
-                                    void* __stack; \
-                                    __asm {mov __stack, esp}; \
-                                    __asm {push FOO_DEFAULT_SIZE}; \
-                                    fooPtr(__VA_ARGS__); \
-                                    __asm {cmp __stack, esp}; \
-                                    __asm {je $ + 12}; \
-                                    __asm {pop eax}; \
-                                    __asm {jmp $ - 10}; \
-                                }
+#define Foo(...)                \
+/* Open new scope              */{\
+/* Create stack variable       */    void* stack__; \
+                                  \
+/* Copy program's stack        */ \
+/* pointer to stack__ variable */   __asm {mov stack__, esp}; \
+                                  \
+/* Inject default argument in  */ \
+/* the program's stack         */   __asm {push FOO_DEFAULT_SIZE}; \
+                                  \
+/* Call function through       */ \
+/* function pointer, with      */ \
+/* variadic arguments          */   fooPtr(__VA_ARGS__); \
+                                  \
+/* Compare current stack       */ \
+/* pointer with copied stack   */ \
+/* pointer (in stack__)        */   __asm {cmp stack__, esp}; \
+                                  \
+/* If stack is back to normal, */ \
+/* skip the following steps    */ \
+/* and return to normal        */ \
+/* program execution           */   __asm {je $ + 12}; \
+                                  \
+/* Pop the stack               */   __asm {pop eax}; \
+                                  \
+/* Return to comparison        */ \
+/* (on line 22)                */   __asm {jmp $ - 10}; \
+                                  \
+/* Exit scope                  */}
 
 extern void(*fooPtr)(void);
 
